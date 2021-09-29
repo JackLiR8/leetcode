6efab146c4cfe0a6966590e74d170781112ed188
JackLiR8 <jiakeli1024@gmail.com>
2021-09-29 11:28:56 +0800

    feat(416): 分割等和子集 空间未优化版

--- src/416.分割等和子集.js
+++ src/416.分割等和子集.js
@@ -25,3 +25,37 @@ var canPartition = function(nums) {
   return dp[sum]
 };
 // @lc code=end
+
+/**
+ * 空间未优化版
+ * @param {number[]} nums 
+ */
+function canPartition(nums) {
+  if (nums.length < 2) return false
+  let sum = nums.reduce((acc, curr) => acc + curr, 0)
+  if (sum % 2 !== 0) return false
+  
+  const m = nums.length,
+        half = sum / 2,
+        dp = Array(m + 1)
+          .fill(0)
+          .map(() => Array(half + 1).fill(false))
+  
+  for (let row of dp) {
+    row[0] = true
+  }
+  
+  // dp[i][j] 的含义是数组前 i 个元素中是否有子集的和为 j
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= half; j++) {
+      if (j - nums[i - 1] < 0) {
+        // 当前元素大于j, dp[j][j] 继承上一行结果
+        dp[i][j] = dp[i - 1][j]
+      } else {
+        // 当前元素小于j, 若上一行为true,则直接为true, 否则依赖dp[i - 1][j - nums[i - 1]]的结果
+        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]]
+      }
+    }
+  }
+  return dp[m][half]
+}
