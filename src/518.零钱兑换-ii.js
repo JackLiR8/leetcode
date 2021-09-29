c90924a5c22aadb38934cde506ceaa4da8137c30
JackLiR8 <jiakeli1024@gmail.com>
2021-09-29 09:26:18 +0800

    feat(518): 零钱兑换新增空间未优化版

--- src/518.零钱兑换-ii.js
+++ src/518.零钱兑换-ii.js
@@ -23,3 +23,33 @@ var change = function(amount, coins) {
   return dp[amount]
 };
 // @lc code=end
+
+/**
+ * 空间未优化版本
+ * @param {number} amount 
+ * @param {number[]} coins 
+ * @returns 
+ */
+function change1(amount, coins) {
+  const m = coins.length
+  const dp = Array(m + 1)
+    .fill(0)
+    .map(() => Array(amount + 1).fill(0))
+
+  for (let row of dp) {
+    row[0] = 1
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= amount; j++) {
+      if (j - coins[i - 1] < 0) {
+        dp[i][j] = dp[i - 1][j]
+      } else {
+        // dp[i][j - coins[i - 1]] 覆盖了当前行的硬币，这是可重复使用的表现
+        dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]]
+      }
+    }
+  }
+
+  return dp[m][amount]
+}
